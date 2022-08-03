1d977956696c4cd5883d7e9ccf82f6f028d1939a
Sergio Sanchez <sergiosan2802@gmail.com>
2022-08-03 18:35:20 -0500

    feat: added img meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,8 +6,10 @@ import { Navbar } from '../ui/navbar'
 
 import styles from './Layout.module.css';
 
+const originPath = (typeof window === 'undefined') ? '' : window.location.origin
 
 export const Layout: FC<ILayout> = ({ children, title }) => {
+
   return (
     <>
       <Head>
@@ -15,9 +17,12 @@ export const Layout: FC<ILayout> = ({ children, title }) => {
         <meta name="author" content="Sergio Sanchez" />
         <meta name="description" content="Informacion sobre el pokemon XXXXXX" />
         <meta name="keywords" content="XXXX, pokemon, pokedex" />
+
+        <meta property="og:title" content={`Informacion sobre ${title}`} />
+        <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+        <meta property="og:image" content={`${originPath}/img/banner.png`} />
       </Head>
 
-      {/* Navbar  */}
       <Navbar />
 
       <main className={styles['main']}>
